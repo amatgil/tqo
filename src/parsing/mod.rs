87a69a3b81fdd30855f4d1a035c653f9a4969b64
amatgil <amatgilvinyes@gmail.com>
2025-09-17 17:24:51 +0200

    Move table out of tests and into code

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,11 +1,14 @@
-//! Bunda-Gerth variation, heavily inspired from [TinyAPL](https://blog.rubenverg.com/tinyapl_4_parsing)
+//! Bunda-Gerth variation, inspired from [TinyAPL](https://blog.rubenverg.com/tinyapl_4_parsing)
+//! and [the paper itself](https://dl.acm.org/doi/pdf/10.1145/384283.801081)
 //! Structured as a tree, where each kind of branch is assigned a Category
 
 use crate::{ast::Sp, *};
+use std::mem;
 mod expr;
 pub(crate) use expr::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
 enum Category {
     /// Array
     A,
@@ -31,22 +34,32 @@ enum Category {
     Ass,
 }
 
-#[rustfmt::skip]
 fn binding_power_of<'a>(a: Category, b: Category) -> Option<(u8, ExprTree<'a>)> {
-    use Category as C;
-    match (a, b) {
-        (C::A, C::A) => todo!(), (C::A, C::Av) => todo!(), (C::A, C::Ov) => todo!(), (C::A, C::Dv) => todo!(), (C::A, C::N) => todo!(), (C::A, C::Aa) => todo!(), (C::A, C::Oa) => todo!(), (C::A, C::Da) => todo!(), (C::A, C::Jot) => todo!(), (C::A, C::Arr) => todo!(), (C::A, C::Ass) => todo!(),
-        (C::Av, C::A) => todo!(), (C::Av, C::Av) => todo!(), (C::Av, C::Ov) => todo!(), (C::Av, C::Dv) => todo!(), (C::Av, C::N) => todo!(), (C::Av, C::Aa) => todo!(), (C::Av, C::Oa) => todo!(), (C::Av, C::Da) => todo!(), (C::Av, C::Jot) => todo!(), (C::Av, C::Arr) => todo!(), (C::Av, C::Ass) => todo!(),
-        (C::Ov, C::A) => todo!(), (C::Ov, C::Av) => todo!(), (C::Ov, C::Ov) => todo!(), (C::Ov, C::Dv) => todo!(), (C::Ov, C::N) => todo!(), (C::Ov, C::Aa) => todo!(), (C::Ov, C::Oa) => todo!(), (C::Ov, C::Da) => todo!(), (C::Ov, C::Jot) => todo!(), (C::Ov, C::Arr) => todo!(), (C::Ov, C::Ass) => todo!(),
-        (C::Dv, C::A) => todo!(), (C::Dv, C::Av) => todo!(), (C::Dv, C::Ov) => todo!(), (C::Dv, C::Dv) => todo!(), (C::Dv, C::N) => todo!(), (C::Dv, C::Aa) => todo!(), (C::Dv, C::Oa) => todo!(), (C::Dv, C::Da) => todo!(), (C::Dv, C::Jot) => todo!(), (C::Dv, C::Arr) => todo!(), (C::Dv, C::Ass) => todo!(),
-        (C::N, C::A) => todo!(), (C::N, C::Av) => todo!(), (C::N, C::Ov) => todo!(), (C::N, C::Dv) => todo!(), (C::N, C::N) => todo!(), (C::N, C::Aa) => todo!(), (C::N, C::Oa) => todo!(), (C::N, C::Da) => todo!(), (C::N, C::Jot) => todo!(), (C::N, C::Arr) => todo!(), (C::N, C::Ass) => todo!(),
-        (C::Aa, C::A) => todo!(), (C::Aa, C::Av) => todo!(), (C::Aa, C::Ov) => todo!(), (C::Aa, C::Dv) => todo!(), (C::Aa, C::N) => todo!(), (C::Aa, C::Aa) => todo!(), (C::Aa, C::Oa) => todo!(), (C::Aa, C::Da) => todo!(), (C::Aa, C::Jot) => todo!(), (C::Aa, C::Arr) => todo!(), (C::Aa, C::Ass) => todo!(),
-        (C::Oa, C::A) => todo!(), (C::Oa, C::Av) => todo!(), (C::Oa, C::Ov) => todo!(), (C::Oa, C::Dv) => todo!(), (C::Oa, C::N) => todo!(), (C::Oa, C::Aa) => todo!(), (C::Oa, C::Oa) => todo!(), (C::Oa, C::Da) => todo!(), (C::Oa, C::Jot) => todo!(), (C::Oa, C::Arr) => todo!(), (C::Oa, C::Ass) => todo!(),
-        (C::Da, C::A) => todo!(), (C::Da, C::Av) => todo!(), (C::Da, C::Ov) => todo!(), (C::Da, C::Dv) => todo!(), (C::Da, C::N) => todo!(), (C::Da, C::Aa) => todo!(), (C::Da, C::Oa) => todo!(), (C::Da, C::Da) => todo!(), (C::Da, C::Jot) => todo!(), (C::Da, C::Arr) => todo!(), (C::Da, C::Ass) => todo!(),
-        (C::Jot, C::A) => todo!(), (C::Jot, C::Av) => todo!(), (C::Jot, C::Ov) => todo!(), (C::Jot, C::Dv) => todo!(), (C::Jot, C::N) => todo!(), (C::Jot, C::Aa) => todo!(), (C::Jot, C::Oa) => todo!(), (C::Jot, C::Da) => todo!(), (C::Jot, C::Jot) => todo!(), (C::Jot, C::Arr) => todo!(), (C::Jot, C::Ass) => todo!(),
-        (C::Arr, C::A) => todo!(), (C::Arr, C::Av) => todo!(), (C::Arr, C::Ov) => todo!(), (C::Arr, C::Dv) => todo!(), (C::Arr, C::N) => todo!(), (C::Arr, C::Aa) => todo!(), (C::Arr, C::Oa) => todo!(), (C::Arr, C::Da) => todo!(), (C::Arr, C::Jot) => todo!(), (C::Arr, C::Arr) => todo!(), (C::Arr, C::Ass) => todo!(),
-        (C::Ass, C::A) => todo!(), (C::Ass, C::Av) => todo!(), (C::Ass, C::Ov) => todo!(), (C::Ass, C::Dv) => todo!(), (C::Ass, C::N) => todo!(), (C::Ass, C::Aa) => todo!(), (C::Ass, C::Oa) => todo!(), (C::Ass, C::Da) => todo!(), (C::Ass, C::Jot) => todo!(), (C::Ass, C::Arr) => todo!(), (C::Ass, C::Ass) => todo!(),
-    }
+    assert!(
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            == [
+                A as u8, Av as u8, Ov as u8, Dv as u8, N as u8, Aa as u8, Oa as u8, Da as u8,
+                Jot as u8, Arr as u8, Ass as u8
+            ]
+    );
+    use Category::*;
+    const TBD: Option<(u8, ExprTree)> = None; // for now!
+
+    #[rustfmt::skip]
+    let table: [[Option<(u8, ExprTree)>; 10]; 10] = [
+              /* A     αV    ⍵V    DV    N     αA   ⍵A    DA    JOT   ARR */
+        /*A*/   [None, TBD,  None, TBD,  None, TBD, TBD,  TBD,  TBD,  TBD],
+        /*αV*/  [None, TBD,  None, TBD,  None, TBD, TBD,  TBD,  TBD,  TBD],
+        /*⍵V*/  [TBD,  None, TBD,  None, TBD,  TBD, None, None, None, TBD],
+        /*DV*/  [TBD,  None, TBD,  None, TBD,  TBD, None, None, None, TBD],
+        /*N*/   [None, TBD,  None, TBD,  None, TBD, TBD,  TBD,  TBD,  TBD],
+        /*αA*/  [None, TBD,  TBD,  TBD,  TBD,  TBD, TBD,  TBD,  TBD,  TBD],
+        /*⍵A*/  [TBD,  TBD,  TBD,  TBD,  TBD,  TBD, TBD,  TBD,  TBD,  TBD],
+        /*DA*/  [TBD,  TBD,  TBD,  TBD,  TBD,  TBD, TBD,  TBD,  TBD,  TBD],
+        /*JOT*/ [TBD,  TBD,  TBD,  TBD,  TBD,  TBD, TBD,  TBD,  TBD,  TBD],
+        /*ARR*/ [TBD,  TBD,  TBD,  TBD,  TBD,  TBD, TBD,  TBD,  TBD,  TBD],
+    ];
+    todo!()
 }
 
 #[derive(Debug, Clone)]
@@ -118,21 +131,6 @@ fn babys_first_parsing() {
 #[test]
 fn bunda_gerth_binding_powers() {
     // See README.md/Bunda-Gerth for the actual table
-    let tbd = None; // for now!
-    #[rustfmt::skip]
-    let table: [[Option<u8>; 10]; 10] = [
-              /* A     αMF   ⍵MF   DF    N     αMM  ⍵MM   DM    JOT   ARR */
-        /*A*/   [None, tbd,  None, tbd,  None, tbd, tbd,  tbd,  tbd,  tbd],
-        /*αMF*/ [None, tbd,  None, tbd,  None, tbd, tbd,  tbd,  tbd,  tbd],
-        /*⍵MF*/ [tbd,  None, tbd,  None, tbd,  tbd, None, None, None, tbd],
-        /*DF*/  [tbd,  None, tbd,  None, tbd,  tbd, None, None, None, tbd],
-        /*N*/   [None, tbd,  None, tbd,  None, tbd, tbd,  tbd,  tbd,  tbd],
-        /*αMM*/ [None, tbd,  tbd,  tbd,  tbd,  tbd, tbd,  tbd,  tbd,  tbd],
-        /*⍵MM*/ [tbd,  tbd,  tbd,  tbd,  tbd,  tbd, tbd,  tbd,  tbd,  tbd],
-        /*DM*/  [tbd,  tbd,  tbd,  tbd,  tbd,  tbd, tbd,  tbd,  tbd,  tbd],
-        /*JOT*/ [tbd,  tbd,  tbd,  tbd,  tbd,  tbd, tbd,  tbd,  tbd,  tbd],
-        /*ARR*/ [tbd,  tbd,  tbd,  tbd,  tbd,  tbd, tbd,  tbd,  tbd,  tbd],
-    ];
 
     use Category::*;
 
@@ -143,9 +141,9 @@ fn bunda_gerth_binding_powers() {
     ];
 
     for (lesser, greater) in gt_conditions {
-        let lesser_prio = table[lesser.0 as u8 as usize][lesser.1 as u8 as usize];
-        let greater_prio = table[greater.0 as u8 as usize][greater.1 as u8 as usize];
+        let lesser_prio = binding_power_of(lesser.0, lesser.1);
+        let greater_prio = binding_power_of(greater.0, greater.1);
 
-        assert!(lesser_prio.is_none() || greater_prio.unwrap() > lesser_prio.unwrap())
+        assert!(lesser_prio.is_none() || greater_prio.unwrap().0 > lesser_prio.unwrap().0)
     }
 }
